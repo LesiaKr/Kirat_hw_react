d7e8f8befb7ecf65987d31c5b63b9ab14f3f0566
Lesia Kirat <lesiakirat@gmail.com>
2023-03-29 20:01:53 +0300

    hw1

---
+++ Kirat_hw1/task_4/src/App.js
@@ -0,0 +1,13 @@
+
+    //  Завдання 4
+//Створіть простий компонент, який міститиме текст і картинку (попередньо додайте нове зображення до проєкту).
+
+function App() {
+  return (
+    <div>
+        <img src="/images/photos.jpg" alt="photo" />
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque modi accusamus repellendus autem a fuga, eos ipsum esse nemo ex quae veniam maiores unde dolore. Quibusdam temporibus provident dolor quo voluptas vitae a praesentium, totam natus fuga rerum, beatae voluptates placeat esse ipsam repellendus, molestias libero doloribus! Expedita, minus ad. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas asperiores a similique id voluptatem? Quasi ea earum, natus magni harum ullam blanditiis adipisci consequatur dolor quidem dicta ducimus recusandae iure. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet officia distinctio vitae in tenetur iste quibusdam, molestias numquam dicta vero odio obcaecati quae perferendis corporis culpa aperiam dolor? Fuga, animi. </p>
+    </div>
+  );
+}
+export default App;
