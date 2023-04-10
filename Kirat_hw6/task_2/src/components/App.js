3e55701e3754bfacaf58f4cebd00dfd6e553d8d7
Lesia Kirat <lesiakirat@gmail.com>
2023-04-10 19:11:05 +0300

    hw6

---
+++ Kirat_hw6/task_2/src/components/App.js
@@ -0,0 +1,12 @@
+import FormComponentSass from './FormComponentSass';
+
+// Завдання 2
+// Створіть компонент форми, додайте підтримку sass у проєкт і стилізуйте форму за допомогою sass.
+
+function App() {
+  return (
+    <FormComponentSass />
+  );
+}
+
+export default App;
---
+++ Kirat_hw6/task_3/src/components/FormComponent.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import styles from './FormComponent.module.css';
+
+class FormComponent extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          login: '',
+          password: '',
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+     
+    handleChange(e) {
+    
+      const { name } = e.target;
+    
+      this.setState({ [name]: e.target.value });
+    }
+    
+    handleSubmit(e) {
+      e.preventDefault();
+      alert("Submit success");
+      this.setState({
+        login: '',
+        password: '',
+      });
+    }
+
+    render(){
+        return(
+            <>
+                <form onSubmit={this.handleSubmit} className={styles['form']}>
+                    <label htmlFor="login">Login: </label>
+                    <input 
+                        type="text" 
+                        name="login"
+                        value={this.state.login} 
+                        onChange={this.handleChange}
+                        required 
+                        minLength={5}
+                        maxLength={16}/>
+                    <br />
+
+                    <label htmlFor="password">Password: </label>
+                    <input 
+                        type="password" 
+                        name="password"
+                        value={this.state.password} 
+                        onChange={this.handleChange}
+                        required 
+                        minLength={6}
+                        maxLength={16}/>
+                    <br />
+
+                    <button>
+                        Submit
+                    </button>
+                </form>
+            </>
+        )
+    }
+}
+export default FormComponent;
